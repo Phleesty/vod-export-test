0b629684f664ae20cc4a5cc36b46ba5aa19ab1d4
Phleesty <72405282+Phleesty@users.noreply.github.com>
2025-09-02 17:43:37 +0300

    replace vk.com -> vk.ru

--- vk.py
+++ vk.py
@@ -177,7 +177,7 @@ def upload_video_to_vk(token, group_id, video_path, album_id, name, description,
         "privacy_view": privacy_view,
         "privacy_comment": "all"
     }
-    response = requests.get("https://api.vk.com/method/video.save", params=params).json()
+    response = requests.get("https://api.vk.ru/method/video.save", params=params).json()
     if "error" in response:
         raise Exception(f"Ошибка VK API: {response['error']['error_msg']}")
     upload_url = response["response"]["upload_url"]
